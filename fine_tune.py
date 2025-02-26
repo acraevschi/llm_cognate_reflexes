0da6d74a2388df3cef616abf924d4233fa792ea7
Alexandru Craevschi <craevschi.alexandru@gmail.com>
2025-02-26 13:59:00 +0100

    upd to use unsloth 8b

--- fine_tune.py
+++ fine_tune.py
@@ -1,32 +1,24 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, EarlyStoppingCallback
 from datasets import load_from_disk
-from peft import LoraConfig, get_peft_model
+
+# from peft import LoraConfig, get_peft_model
 from trl import SFTTrainer, SFTConfig, DataCollatorForCompletionOnlyLM
-from config import HF_TOKEN
+import torch
+
+from unsloth import FastLanguageModel
+from unsloth import is_bfloat16_supported
 
 seed_num = 97
 
-max_length = 12000
+max_length = 12288
 
 
-model = AutoModelForCausalLM.from_pretrained(
-    "./llama-3.2-1B",  # token=HF_TOKEN
-)  # temporary use 1B
-
-if hasattr(model, "enable_input_require_grads"):
-    model.enable_input_require_grads()
-else:
-
-    def make_inputs_require_grad(module, input, output):
-        output.requires_grad_(True)
-
-    model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
-
-tokenizer = AutoTokenizer.from_pretrained(
-    "./llama-3.2-1B",
-    max_length=max_length,
-    # token=HF_TOKEN,
-)  # temporary use 1B
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name="unsloth/Meta-Llama-3.1-8B-bnb-4bit",
+    max_seq_length=max_length,
+    dtype=None,
+    load_in_4bit=True,
+)
 
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
@@ -37,21 +29,31 @@ data = load_from_disk("hf_cognates_dataset").shuffle(seed=seed_num)
 val_inds = data.num_rows // 10
 train_data = data.select(range(val_inds, data.num_rows))
 val_data = data.select(range(0, val_inds))
-val_inds = data.num_rows // 10
 
 del data
 
-# Need to double check the LoraConfig arguments
-lora_config = LoraConfig(
-    r=8,
+model = FastLanguageModel.get_peft_model(
+    model,
+    r=16,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+    ],
     lora_alpha=32,
-    target_modules=["q_proj", "v_proj"],
-    lora_dropout=0.1,
-    bias="none",
-    task_type="CAUSAL_LM",
+    lora_dropout=0,  # Supports any, but = 0 is optimized
+    bias="none",  # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+    random_state=seed_num,
+    max_seq_length=max_length,
+    use_rslora=False,  # We support rank stabilized LoRA
+    loftq_config=None,  # And LoftQ
 )
-
-model = get_peft_model(model, lora_config)
 
 
 def formatting_prompts_func(example):
@@ -78,30 +80,30 @@ early_stopping = EarlyStoppingCallback(
 )
 
 training_args = SFTConfig(
-    output_dir="./sft_1b",
+    output_dir="./sft_8b",
     overwrite_output_dir=True,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    gradient_accumulation_steps=4,
-    gradient_checkpointing=True,
-    optim="adamw_bnb_8bit",
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
+    gradient_accumulation_steps=32,
+    eval_accumulation_steps=32,
+    optim="adamw_8bit",
     eval_strategy="steps",
     eval_steps=500,
     save_steps=500,
     logging_steps=500,
+    warmup_ratio=0.1,
     learning_rate=5e-6,
-    num_train_epochs=1,
+    num_train_epochs=2,
     seed=seed_num,
     label_smoothing_factor=0.05,
     neftune_noise_alpha=5,
     load_best_model_at_end=True,
     metric_for_best_model="eval_loss",
     max_seq_length=max_length,
-    torch_compile=True,
-    # bf16=True,
+    # torch_compile=True,
+    bf16=is_bfloat16_supported(),
     dataloader_num_workers=8,
 )
-
 
 trainer = SFTTrainer(
     model=model,
@@ -117,7 +119,7 @@ trainer = SFTTrainer(
 trainer.train()
 
 # Define the path to save the best model
-best_model_path = "./sft_1b_best"
+best_model_path = "./sft_8b_best"
 
 # Save the model
-trainer.model.save_pretrained(best_model_path)
+trainer.save_model(best_model_path)
